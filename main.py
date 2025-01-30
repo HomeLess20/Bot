621a1e7063591ad4c467480e361bfdb1186eff06
HomeLess20 <torontotokyoboy@gmail.com>
2025-01-30 20:47:27 +0700

    BOT Discrod

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import discord
+from discord.ext import commands
+from discrod import app_commands
+
+from server import server_on
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+
+# ส่วนของการเรียกใช้งานคำสั่ง
+
+
+server_on()
+
+bot.run(os.getenv('TOKEN'))
